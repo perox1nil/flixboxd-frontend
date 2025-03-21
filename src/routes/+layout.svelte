d08f6fb49ee76e9621aaddcc529175f9ce5aefc9
aybancaughtin4k <203548283+aybancaughtin4k@users.noreply.github.com>
2025-03-21 15:09:47 +0800

    chore: update dependencies

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,27 @@
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
+	import { browser } from '$app/environment';
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+	import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools';
+	import { Toaster } from 'svelte-french-toast';
+
 	let { children } = $props();
+
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser
+			}
+		}
+	});
 </script>
 
-<ModeWatcher />
-{@render children()}
+<Toaster />
+
+<QueryClientProvider client={queryClient}>
+	<ModeWatcher />
+
+	{@render children()}
+	<SvelteQueryDevtools />
+</QueryClientProvider>
